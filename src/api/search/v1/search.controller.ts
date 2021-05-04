ce87b8be69638bc5b78d8e2e8e2ccd5c01846468
aMahanna <anthony.mahanna@gmail.com>
2021-05-03 21:32:40 -0400

    rework Search API

--- src/api/search/v1/search.controller.ts
+++ src/api/search/v1/search.controller.ts
@@ -2,14 +2,10 @@ import { Request, Response } from 'express';
 import { imageObject } from '../../../collections/Image';
 
 namespace SearchController {
-  export async function from_author(req: Request, res: Response): Promise<void> {
-    const author: string = req.body.author || '%';
-    const result: {}[] = await imageObject.simpleQuery('author', author);
+  export async function from_mixed_keys(req: Request, res: Response): Promise<void> {
+    const Labels: string[] = typeof req.query.labels === 'string' ? req.query.labels.split(' ') : [''];
+    const result: {}[] = await imageObject.query_mixed_keys(Labels);
     res.status(200).json(result);
-  }
-
-  export async function ping(req: Request, res: Response): Promise<void> {
-    res.status(200).json('pong');
   }
 }
 
--- src/api/search/v1/search.router.ts
+++ src/api/search/v1/search.router.ts
@@ -2,6 +2,5 @@ import search from './search.controller';
 import { Express } from 'express';
 
 export default function (app: Express): void {
-  app.route('/api/search/author').post(search.from_author);
-  app.route('/api/search/ping').get(search.ping);
+  app.route('/api/search/mixed').get(search.from_mixed_keys);
 }

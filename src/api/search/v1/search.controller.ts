34a4ddb308fb5737d229d85467ed2f09f979814d
aMahanna <anthony.mahanna@gmail.com>
2021-05-04 16:08:49 -0400

    new: GET /randomkeys endpoint

--- src/api/search/v1/search.controller.ts
+++ src/api/search/v1/search.controller.ts
@@ -13,15 +13,22 @@ namespace SearchController {
   }
 
   export async function from_surprise_keys(req: Request, res: Response): Promise<void> {
-    const Labels: string[] | undefined = await imageObject.fetch_surprise_keys();
-    console.log(Labels);
-    if (!Labels) res.status(500).json('Error fetching surprise keys');
-    if (Labels) {
+    const labels: string[] | undefined = await imageObject.fetch_surprise_keys();
+    if (!labels) res.status(500).json('Error fetching surprise keys');
+    if (labels) {
       const result: {}[] | undefined = await (req.query.isStrict
-        ? imageObject.query_mixed_keys_strict(Labels)
-        : imageObject.query_mixed_keys(Labels));
-      if (!result) res.status(500).json('Error searching from mixed keys');
-      res.status(200).json({ result, Labels });
+        ? imageObject.query_mixed_keys_strict(labels)
+        : imageObject.query_mixed_keys(labels));
+      if (!result) res.status(500).json('Error searching from surprise keys');
+      res.status(200).json({ result, labels });
+    }
+  }
+
+  export async function fetch_surprise_keys(req: Request, res: Response): Promise<void> {
+    const labels: string[] | undefined = await imageObject.fetch_surprise_keys();
+    if (!labels) res.status(500).json('Error fetching surprise keys');
+    if (labels) {
+      res.status(200).json({ labels });
     }
   }
 }

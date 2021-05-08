ae3d8da13891ee34d224fb122c6691fcd12ca7b7
aMahanna <anthony.mahanna@gmail.com>
2021-05-08 12:48:39 -0400

    new: user can select favourite images
    this will be used in the upcoming Discover feature

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -7,9 +7,15 @@ import { Container, Button, Table, TableBody, TableContainer, TableHead, TableRo
 const History = () => {
   const [t, i18n] = useTranslation();
   const [makeHistory, setMakeHistory] = useState(''); // Provide some random labels for a quick search
+
   const [history] = useState(() => {
-    // Fetch the history cache values
     const persistedState = localStorage.getItem('data');
+    const persistedData = persistedState ? JSON.parse(persistedState) : {};
+    return Object.keys(persistedData).length === 0 ? undefined : Object.entries(persistedData).map(e => ({ [e[0]]: e[1] }));
+  });
+  const [favourites] = useState(() => {
+    // Fetch the history cache values
+    const persistedState = localStorage.getItem('favourites');
     const persistedData = persistedState ? JSON.parse(persistedState) : {};
     return Object.keys(persistedData).length === 0 ? undefined : Object.entries(persistedData).map(e => ({ [e[0]]: e[1] }));
   });
@@ -35,6 +41,7 @@ const History = () => {
   const clearCache = async () => {
     localStorage.removeItem('data');
     localStorage.removeItem('lastSearch');
+    localStorage.removeItem('favourites');
     window.location.reload();
   };
 
@@ -45,18 +52,35 @@ const History = () => {
    */
   const HistoryRow = (props: { persistedState: any }) => {
     const key: string = Object.keys(props.persistedState)[0];
-    const search: string = key;
     const date: Date = new Date(props.persistedState[key].date);
     const results: number = props.persistedState[key].data.length;
     return (
-      <TableRow key={search}>
+      <TableRow key={key}>
         <TableCell component="th" scope="row">
-          {search}
+          {key}
         </TableCell>
-        <TableCell align="right">{`${date.toLocaleTimeString(i18n.language)} (${date.toLocaleDateString(i18n.language)})`}</TableCell>
-        <TableCell align="right">{results}</TableCell>
-        <TableCell align="right" className="search">
+        <TableCell align="center">{results}</TableCell>
+        <TableCell align="center">{`${date.toLocaleTimeString(i18n.language)} (${date.toLocaleDateString(i18n.language)})`}</TableCell>
+        <TableCell align="center" className="search">
           <Link to={{ pathname: '/search', state: { fromHistory: key } }}>{t('historyPage.view')}</Link>
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  const FavouritesRow = (props: { persistedState: any }) => {
+    const key: string = Object.keys(props.persistedState)[0];
+    const date: Date = new Date(props.persistedState[key].date);
+    const author: string = props.persistedState[key].author;
+    return (
+      <TableRow key={key}>
+        <TableCell component="th" scope="row">
+          {key}
+        </TableCell>
+        <TableCell align="center">{author}</TableCell>
+        <TableCell align="center">{`${date.toLocaleTimeString(i18n.language)} (${date.toLocaleDateString(i18n.language)})`}</TableCell>
+        <TableCell align="center" className="search">
+          <Link to={{ pathname: `/info/${key}` }}>{t('historyPage.view')}</Link>
         </TableCell>
       </TableRow>
     );
@@ -69,31 +93,54 @@ const History = () => {
           <Link to={{ pathname: '/search', state: { fromHistory: makeHistory } }}>{t('historyPage.makehistory')}</Link>
         </div>
       )}
+      {(history || favourites) && (
+        <Button id="history-clear" style={{ color: '#2f2d2e' }} onClick={clearCache}>
+          {t('historyPage.clear')}
+        </Button>
+      )}
       {history && (
-        <div>
-          <Button id="history-clear" style={{ color: '#2f2d2e' }} onClick={clearCache}>
-            {t('historyPage.clear')}
-          </Button>
-          <Box mt={3}>
-            <TableContainer component={Paper}>
-              <Table aria-label="history-table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>{t('historyPage.search')}</TableCell>
-                    <TableCell align="center">{t('historyPage.date')}</TableCell>
-                    <TableCell align="center">{t('historyPage.results')}</TableCell>
-                    <TableCell align="center">{t('historyPage.view')}</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {history.map(elem => (
-                    <HistoryRow key={Object.keys(elem)[0]} persistedState={elem} />
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
-        </div>
+        <Box mt={3}>
+          <h3>{t('historyPage.searches')}</h3>
+          <TableContainer component={Paper} style={{ maxHeight: '32vh' }}>
+            <Table aria-label="history-table" stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{t('historyPage.search')}</TableCell>
+                  <TableCell align="center">{t('historyPage.results')}</TableCell>
+                  <TableCell align="center">{t('historyPage.date')}</TableCell>
+                  <TableCell align="center">{t('historyPage.view')}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {history.map(elem => (
+                  <HistoryRow key={Object.keys(elem)[0]} persistedState={elem} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
+      {favourites && (
+        <Box mt={3}>
+          <h3>{t('historyPage.favourites')}</h3>
+          <TableContainer component={Paper} style={{ maxHeight: '32vh' }}>
+            <Table aria-label="history-table" stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{t('historyPage.imageID')}</TableCell>
+                  <TableCell align="center">{t('historyPage.author')}</TableCell>
+                  <TableCell align="center">{t('historyPage.date')}</TableCell>
+                  <TableCell align="center">{t('historyPage.view')}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {favourites.map(elem => (
+                  <FavouritesRow key={Object.keys(elem)[0]} persistedState={elem} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       )}
     </Container>
   );

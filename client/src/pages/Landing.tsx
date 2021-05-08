d49f0785306be6c8c6c7fcc1dff1b0dba81eb461
aMahanna <anthony.mahanna@gmail.com>
2021-05-08 12:49:10 -0400

    update landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 // Import MUI Components
-import WhereToVoteOutlinedIcon from '@material-ui/icons/WhereToVoteOutlined';
+import HomeIcon from '@material-ui/icons/BubbleChart';
 import { Container, CssBaseline, Box, makeStyles, Avatar, createStyles } from '@material-ui/core';
 import { default as MUILink } from '@material-ui/core/Link';
 // Import Props interface to define what this component can receive as props
@@ -36,10 +36,14 @@ const Landing = () => {
     <Container component="main" maxWidth="sm">
       <CssBaseline />
       <Avatar className={classes.avatar}>
-        <WhereToVoteOutlinedIcon fontSize="large" />
+        <HomeIcon fontSize="large" />
       </Avatar>
       <Box mt={4}>
         <h2>{t('landingPage.heading')}</h2>
+        <h3>{t('landingPage.description')}</h3>
+        <p>{t('landingPage.discover')}</p>
+        <p>{t('landingPage.visualize')}</p>
+        <p>{t('landingPage.info')}</p>
       </Box>
       <Box mt={4}>
         <p>
